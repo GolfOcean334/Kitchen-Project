8c6227373c467d70873d4b1404732d1217acf248
Axel Gathelier <130740013+GolfOcean334@users.noreply.github.com>
2024-12-12 12:09:15 +0100

    Minor: Visualisation de la temperature

---
+++ My project/Assets/Script/Stove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stove : MonoBehaviour
+{
+    [SerializeField] private Slider stoveSlider;
+    
+    void Update()
+    {
+        PrintStoveSliderValue();
+    }
+
+    private void PrintStoveSliderValue()
+    {
+        Debug.Log((int)stoveSlider.value + " °C");
+    }
+}
